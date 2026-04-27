1151c83664eee708958e3bb375cef51ee199082d
silverThunder09 <jeg9017@gmail.com>
2026-04-27 13:02:40 +0900

    feat: 고객 초기 데이터 생성 기능 추가
    - 주문 테스트용 고객 더미 데이터 추가
    - 고객 상태별 초기 데이터 구성

--- src/main/java/com/sparta/cch/backofficeproject/customer/entity/Customer.java
+++ src/main/java/com/sparta/cch/backofficeproject/customer/entity/Customer.java
@@ -1,4 +1,39 @@
 package com.sparta.cch.backofficeproject.customer.entity;
 
-public class Customer {
+import com.sparta.cch.backofficeproject.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Customer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CustomerStatus status;
+
+    public Customer(String name, String email, String phoneNumber, CustomerStatus status) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.status = status;
+    }
 }
+
